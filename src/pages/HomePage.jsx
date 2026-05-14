3e3c92ef169a1fb129a2eb756645f69135b50916
PraptiH <praptiantara1892@gmail.com>
2026-05-14 21:52:33 +0600

    Make some product cart in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,11 @@ import logo from '../assets/logo-white.png'
 import mobileLogo from '../assets/mobile-logo-white.png'
 import searchIcon from '../assets/search-icon.png'
 import cartIcon from '../assets/cart-icon.png'
+import socks from '../assets/athletic-cotton-socks-6-pairs.jpg'
+import basketball from '../assets/intermediate-composite-basketball.jpg'
+import cottonShirt from '../assets/adults-plain-cotton-tshirt-2-pack-teal.jpg'
+import rating45 from '../assets/rating-45.png'
+import rating40 from '../assets/rating-40.png'
 import './HomePage.css'
 
 function HomePage() {
@@ -9,7 +14,7 @@ function HomePage() {
         <>
             <div className="header">
                 <div className="left-section">
-                    <a className="header-link" href="">
+                    <a className="header-link" href="/">
                         <img className='logo' src={logo} />
                         <img className="mobile-logo" src={mobileLogo} />
                     </a>
@@ -33,6 +38,56 @@ function HomePage() {
                         <div className="cart-text">Cart</div>
                     </a>
                 </div>
+            </div>
+
+            <div className='cart'>
+
+                <div className='cart-info'>
+
+                    <img className='product-img' src={socks} alt="" />
+
+                    <div className='cart-details'>
+                        <h4 className='cart-text'>Black and Gray Athletic Cotton Socks - 6 Pairs</h4>
+
+                        <img className='rating-img' src={rating45} alt="" />
+                        <p className='product-price'>$10.90</p>
+                        <button className='button-secondary'>1</button>
+
+                        <button className='button-primary'>Add to Cart</button>
+                    </div>
+
+                </div>
+
+                <div className='cart-info'>
+
+                    <img className='product-img' src={basketball} alt="" />
+
+                    <div className='cart-details'>
+                        <h4 className='cart-text'>Intermediate Size Basketball</h4>
+
+                        <img className='rating-img' src={rating45} alt="" />
+                        <p className='product-price'>$20.95</p>
+                        <button className='button-secondary'>1</button>
+
+                        <button className='button-primary'>Add to Cart</button>
+                    </div>
+                </div>
+
+                <div className='cart-info'>
+
+                    <img className='product-img' src={cottonShirt} alt="" />
+
+                    <div className='cart-details'>
+                        <h4 className='cart-text'>Adults Plain Cotton T-Shirt - 2 Pack</h4>
+
+                        <img className='rating-img' src={rating40} alt="" />
+                        <p className='product-price'>$20.95</p>
+                        <button className='button-secondary'>1</button>
+
+                        <button className='button-primary'>Add to Cart</button>
+                    </div>
+                </div>
+
             </div>
         </>
     )
