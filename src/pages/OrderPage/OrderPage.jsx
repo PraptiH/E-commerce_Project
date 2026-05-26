abb55ab1213d4b07b76e1f8b5350ee15be34f005
PraptiH <praptiantara1892@gmail.com>
2026-05-26 16:17:05 +0600

    Get the data for order page from backend

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,64 +1,73 @@
-import logo from '../../assets/logo-white.png'
-import mobileLogo from '../../assets/mobile-logo-white.png'
-import searchIcon from '../../assets/search-icon.png'
 import cartIcon from '../../assets/cart-icon.png'
-import socks from '../../assets/athletic-cotton-socks-6-pairs.jpg'
-import basketball from '../../assets/intermediate-composite-basketball.jpg'
-import './OrderPage.css'
 import Header from '../../components/Header/Header'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import dayjs from 'dayjs'
+import './OrderPage.css'
+import formateMoney from '../../utilities/money'
 
 
-function OrderPage({cart}) {
+function OrderPage({ cart }) {
+
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/orders?expand=products')
+            .then((response) => {
+                setOrders(response.data)
+            })
+    }, [])
+
     return (
         <>
             <title>Order Page</title>
-            
-            <Header cart={cart}/>
+
+            <Header cart={cart} />
 
             <div className='orderPage'>
                 <h2>Your orders</h2>
-                <div className='orderInfo'>
-                    <div className='orderInfo-leftSide'>
-                        <h5 className='orderInfo-heading'>Order placed : <span>May 21</span></h5>
-                        <h5 className='orderInfo-heading'>Total : <span>$35.02</span></h5>
-                    </div>
-                    <h5 className='orderInfo-heading'>Order Id : <span>27cba69d-4c3d-4098-b42d-ac7fa62b7664</span></h5>
-                </div>
-
-                    <div className='orderCard'>
-                        <div className='orderCardDetail'>
-                            <div className='orderCard-leftside'>
-                                <img className='productImg' src={socks} alt="" />
-                                <div className='productDetail'>
-                                    <p className='productName'>Black and Gray Athletic Cotton Socks - 6 Pairs</p>
-                                    <p>Arriving on: May 26</p>
-                                    <p>Quantity: 1</p>
-                                    <button className='btn-primary'>
-                                        <img className="cart-icon" src={cartIcon} alt="" />
-                                        <p>Add to cart</p>
-                                    </button>
+                {
+                    orders.map((order) => {
+                        return (
+                            <>
+                                <div key={order.id} className='orderInfo'>
+                                    <div className='orderInfo-leftSide'>
+                                        <h5 className='orderInfo-heading'>Order placed : <span>{dayjs(order.orderTimeMs).format('dddd, MMMM D')}</span></h5>
+                                        <h5 className='orderInfo-heading'>Total : <span>{formateMoney(order.totalCostCents)}</span></h5>
+                                    </div>
+                                    <h5 className='orderInfo-heading'>Order Id : <span>{order.id}</span></h5>
                                 </div>
-                            </div>
-                            <button className='trackBtn'>Track Package</button>
-                        </div>
 
-                        <div className='orderCardDetail'>
-                            <div className='orderCard-leftside'>
-                                <img className='productImg' src={basketball} alt="" />
-                                <div className='productDetail'>
-                                    <p className='productName'>Black and Gray Athletic Cotton Socks - 6 Pairs</p>
-                                    <p>Arriving on: May 26</p>
-                                    <p>Quantity: 1</p>
-                                    <button className='btn-primary'>
-                                        <img className="cart-icon" src={cartIcon} alt="" />
-                                        <p>Add to cart</p>
-                                    </button>
+                                <div className='orderCard'>
+                                    {order.products.map((orderProduct) => {
+                                        return (
+                                            <>
+                                                <div key={orderProduct.product.id} className='orderCardDetail'>
+                                                    <div className='orderCard-leftside'>
+                                                        <img className='productImg' src={`/${orderProduct.product.image}`} alt={orderProduct.product.name} />
+                                                        <div className='productDetail'>
+                                                            <p className='productName'>{orderProduct.product.name}</p>
+                                                            <p>Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}</p>
+                                                            <p>Quantity: {orderProduct.quantity}</p>
+                                                            <button className='btn-primary'>
+                                                                <img className="cart-icon" src={cartIcon} alt="" />
+                                                                <p>Add to cart</p>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                    <button className='trackBtn'>Track Package</button>
+                                                </div>
+                                            </>
+                                        )
+                                    })}
+
+
                                 </div>
-                            </div>
-                            <button className='trackBtn'>Track Package</button>
-                        </div>
-                    </div>
-                
+                            </>
+                        )
+                    })
+                }
+
             </div>
         </>
     )
